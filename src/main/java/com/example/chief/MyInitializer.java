b682732294acf3fbd1f1a4aef1b350f0d8794ae7
formidablechief_27 <dhrumilstarwars@gmail.com>
2024-02-22 16:02:24 +0530

    f

--- src/main/java/com/example/chief/MyInitializer.java
+++ src/main/java/com/example/chief/MyInitializer.java
@@ -1,4 +1,5 @@
 package com.example.chief;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -14,39 +15,38 @@ import javax.annotation.PostConstruct;
 
 @Component
 public class MyInitializer {
-	
-	private QuestionsRepository questions_repository;
-	private TestsRepository tests_repository;
-	
-	@Autowired
+
+    private QuestionsRepository questions_repository;
+    private TestsRepository tests_repository;
+
+    @Autowired
     public MyInitializer(QuestionsRepository questionsRepository, TestsRepository tests_Repository) {
         this.questions_repository = questionsRepository;
         this.tests_repository = tests_Repository;
     }
-	
-	public List<Questions> getAllQuestions() {
+
+    public List<Questions> getAllQuestions() {
         return questions_repository.findAll();
     }
-	
-	public List<Tests> getAllTestCases() {
+
+    public List<Tests> getAllTestCases() {
         return tests_repository.findAll();
     }
 
-	@PostConstruct
+    @PostConstruct
     public void initialize() {
         Thread initializationThread = new Thread(() -> {
-        	List<Questions> list = getAllQuestions();
-            for(Questions Q : list) {
-            	int id = Q.getId();
-            	DataCache.ques_map.put(id, Q);
+            List<Questions> list = getAllQuestions();
+            for (Questions Q : list) {
+                int id = Q.getId();
+                DataCache.ques_map.put(id, Q);
             }
             List<Tests> list2 = getAllTestCases();
-            for(Tests t : list2) {
-            	int id = t.getTestcaseNumber();
-            	DataCache.test_map.put(id, t);
+            for (Tests t : list2) {
+                int id = t.getTestcaseNumber();
+                DataCache.test_map.put(id, t);
             }
         });
         initializationThread.start();
     }
 }
-
