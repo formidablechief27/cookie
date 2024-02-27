6ea7d38a749892e94ac1b4cf459e3f70f640e51a
formidablechief_27 <dhrumilstarwars@gmail.com>
2024-02-27 15:27:51 +0530

    f

--- src/main/java/com/example/chief/MyInitializer.java
+++ src/main/java/com/example/chief/MyInitializer.java
@@ -4,9 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.example.chief.model.Questions;
+import com.example.chief.model.Submissions;
 import com.example.chief.model.Tests;
 import com.example.chief.repository.DataCache;
 import com.example.chief.repository.QuestionsRepository;
+import com.example.chief.repository.SubmissionsRepository;
 import com.example.chief.repository.TestsRepository;
 
 import java.util.List;
@@ -18,11 +20,13 @@ public class MyInitializer {
 
     private QuestionsRepository questions_repository;
     private TestsRepository tests_repository;
+    private SubmissionsRepository subs_repo;
 
     @Autowired
-    public MyInitializer(QuestionsRepository questionsRepository, TestsRepository tests_Repository) {
+    public MyInitializer(QuestionsRepository questionsRepository, TestsRepository tests_Repository, SubmissionsRepository subs_repo) {
         this.questions_repository = questionsRepository;
         this.tests_repository = tests_Repository;
+        this.subs_repo = subs_repo;
     }
 
     public List<Questions> getAllQuestions() {
@@ -31,6 +35,10 @@ public class MyInitializer {
 
     public List<Tests> getAllTestCases() {
         return tests_repository.findAll();
+    }
+    
+    public List<Submissions> get() {
+    	return subs_repo.findAll();
     }
 
     @PostConstruct
@@ -45,6 +53,11 @@ public class MyInitializer {
             for (Tests t : list2) {
                 int id = t.getTestcaseNumber();
                 DataCache.test_map.put(id, t);
+            }
+            List<Submissions> list3 = get();
+            for(Submissions s : list3) {
+            	int id = s.getId();
+            	DataCache.sub_map.put(id, s);
             }
         });
         initializationThread.start();
